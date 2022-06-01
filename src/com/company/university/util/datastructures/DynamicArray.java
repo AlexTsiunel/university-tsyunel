6a6f0ad69fe75190c9dc87c931645fa14f352bc0
dev_ats_h <alextsiunel.work@gmail.com>
2022-06-01 19:26:38 +0300

    Modified DynamicArray.java

--- src/com/company/university/util/datastructures/DynamicArray.java
+++ src/com/company/university/util/datastructures/DynamicArray.java
@@ -1,12 +1,15 @@
 package com.company.university.util.datastructures;
 
 public interface DynamicArray {
-    int size(); //возвращает количество элементов в структуре
-    boolean add(Object obj); //добавл€ет в структуру указанный объект
-    boolean remove(Object obj); //удал€ет из структуры указанный объект
-    boolean contains(Object obj); //провер€ет наличие объекта в структуре
-    Object get(int index); //возвращает элемент из структуры с соответствующим пор€дковым номером
-    Object[] toArray(); //возвращает массив с объектами вход€щими в структуру (без пустых €чеек)
+    int size();
 
+    boolean add(Object obj);
 
+    boolean remove(Object obj);
+
+    boolean contains(Object obj);
+
+    Object get(int index);
+
+    Object[] toArray();
 }
